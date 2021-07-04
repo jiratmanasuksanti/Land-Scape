54e85bc8b1e97ba67ad260e504fc8bc5e4937d70
jiratmanasuksanti <86285957+jiratmanasuksanti@users.noreply.github.com>
2021-07-04 10:16:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ screen = pygame.display.set_mode((Width,Height),pygame.FULLSCREEN)
 title = pygame.display.set_caption("LandScape")
 icon = pygame.image.load("icon.png")
 pygame.display.set_icon(icon)
+gameloop = True
+#Import Module#
+from menu_data import *
+
+while gameloop:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      gameloop = False
