5a535b32e6af074fa7beff6e10d35219dd927479
jiratmanasuksanti <86285957+jiratmanasuksanti@users.noreply.github.com>
2021-07-04 10:22:18 +0700

    Create menu_data.py

---
+++ menu_data.py
@@ -0,0 +1,6 @@
+import pygame
+start_button_image = pygame.image.load("lib/assets/images/start_button.png")
+start_buttonx = 680
+start_buttony = 400
+def show_menu():
+  screen.blit(start_button_image,(start_buttonx,start_buttony))
